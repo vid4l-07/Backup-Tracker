2a1c7df01236c87e8121256734458b5f33a7cbb5
Hugo Vidal <hugovima09@gmail.com>
2026-09-18 17:58:54 +0200

    feat: add commands to fix errors

--- 10-launch.sh
+++ 10-launch.sh
@@ -3,5 +3,6 @@
 # /etc/NetworkManager/dispatcher.d/10-launch.sh
 
 if [[ "$2" == "up" ]]; then
-	systemctl start backup.service
+	# systemctl start backup.service  # with logs (journalctl -u backup.service)
+	runuser -u hvidal -- /home/hvidal/datos_lin/programacion/scripts/backup_tracker/backup_tracker.sh  # without but more simple
 fi
